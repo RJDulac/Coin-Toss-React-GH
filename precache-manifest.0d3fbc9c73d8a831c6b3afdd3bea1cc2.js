66c8471d9113d04cc4eb8b59b0ec29b9e0440c11
Ryan Dulac <analyticrye@gmail.com>
2019-07-09 14:22:41 -0400

    Updates

---
+++ precache-manifest.0d3fbc9c73d8a831c6b3afdd3bea1cc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3570a3e09adf4c23fc402426fcb4dd9b",
+    "url": "/Coin-Toss-React-GH/index.html"
+  },
+  {
+    "revision": "1979b4cb615248b15747",
+    "url": "/Coin-Toss-React-GH/static/css/main.544c0aec.chunk.css"
+  },
+  {
+    "revision": "49fe2bb6337391b45054",
+    "url": "/Coin-Toss-React-GH/static/js/2.6e39e941.chunk.js"
+  },
+  {
+    "revision": "1979b4cb615248b15747",
+    "url": "/Coin-Toss-React-GH/static/js/main.c843f5dd.chunk.js"
+  },
+  {
+    "revision": "7db2eec98121ef5becaf",
+    "url": "/Coin-Toss-React-GH/static/js/runtime~main.9b6f0102.js"
+  },
+  {
+    "revision": "d1fcead49e6b31d46a1d4af065f2a60b",
+    "url": "/Coin-Toss-React-GH/static/media/heads.d1fcead4.jpg"
+  },
+  {
+    "revision": "3fb05e9d4dac937be3d15c2ee5a5dfc2",
+    "url": "/Coin-Toss-React-GH/static/media/tails.3fb05e9d.jpg"
+  }
+]);
